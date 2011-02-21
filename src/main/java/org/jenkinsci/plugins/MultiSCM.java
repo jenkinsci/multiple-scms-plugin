96978d2f78f1e9dd227967cb4a14b749fc246084
Kevin Bell <kbell6397@gmail.com>
2011-02-21 16:58:28 -0600

    More experimental hacking.
    -- Simple checkouts appear to work
    -- Working on getting the post-checkout changelog stuff going

--- src/main/java/org/jenkinsci/plugins/MultiSCM.java
+++ src/main/java/org/jenkinsci/plugins/MultiSCM.java
@@ -3,6 +3,8 @@ import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.BuildListener;
+import hudson.model.Descriptor;
+import hudson.model.Saveable;
 import hudson.model.TaskListener;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
@@ -13,11 +15,14 @@ import hudson.scm.SCMRevisionState;
 import hudson.scm.NullSCM;
 import hudson.scm.SCM;
 import hudson.tasks.Builder;
+import hudson.util.DescribableList;
 import hudson.util.FormValidation;
-import hudson.util.ListBoxModel;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,9 +30,11 @@ import javax.servlet.ServletException;
 
 import net.sf.json.JSONObject;
 
+import org.apache.commons.io.FileUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.export.Exported;
 
 /**
  * Sample {@link Builder}.
@@ -46,11 +53,21 @@ import org.kohsuke.stapler.StaplerRequest;
  *
  * @author Kohsuke Kawaguchi
  */
-public class MultiSCM extends SCM {
+public class MultiSCM extends SCM implements Saveable {
 
-    public MultiSCM() {
+    private DescribableList<SCM,Descriptor<SCM>> scms =
+        new DescribableList<SCM,Descriptor<SCM>>(this);
+    
+	@DataBoundConstructor
+    public MultiSCM(List<SCM> scmList) throws IOException {
+		scms.addAll(scmList);
     }
-
+	
+	@Exported
+	public List<SCM> getConfiguredSCMs() {
+		return scms.toList();
+	}
+	
     @Override
 	public SCMRevisionState calcRevisionsFromBuild(AbstractBuild<?, ?> build,
 			Launcher launcher, TaskListener listener) throws IOException,
@@ -72,20 +89,37 @@ public class MultiSCM extends SCM {
 	public boolean checkout(AbstractBuild<?, ?> build, Launcher launcher,
 			FilePath workspace, BuildListener listener, File changelogFile)
 			throws IOException, InterruptedException {
-		// TODO Auto-generated method stub
-		return false;
+
+		FileOutputStream logStream = new FileOutputStream(changelogFile);
+		OutputStreamWriter logWriter = new OutputStreamWriter(logStream);
+		logWriter.write(String.format("<%s>\n", MultiSCMChangeLogParser.ROOT_XML_TAG));
+		
+		boolean checkoutOK = true;		
+		for(SCM scm : scms) {
+			String changeLogPath = changelogFile.getPath() + ".temp";
+			File subChangeLog = new File(changeLogPath);
+			checkoutOK = scm.checkout(build, launcher, workspace, listener, subChangeLog) && checkoutOK;
+			
+			String subLogText = FileUtils.readFileToString(subChangeLog);
+			logWriter.write(String.format("<%s scm=\"%s\">\n<![CDATA[%s]]>\n</%s>\n",
+					MultiSCMChangeLogParser.SUB_LOG_TAG,
+					scm.getClass().getName(),
+					subLogText,
+					MultiSCMChangeLogParser.SUB_LOG_TAG));
+			
+			subChangeLog.delete();
+		}
+		logWriter.write(String.format("</%s>\n", MultiSCMChangeLogParser.ROOT_XML_TAG));
+		logWriter.close();
+
+		return checkoutOK;
 	}
 
 	@Override
 	public ChangeLogParser createChangeLogParser() {
-		// TODO Auto-generated method stub
-		return null;
+		return new MultiSCMChangeLogParser(scms.toList());
 	}
 
-	public List<SCMDescriptor<?>> getScms() {
-		return new ArrayList<SCMDescriptor<?>>();
-	}
-    	
 	/**
 	 * Descriptor for {@link MultiSCM}. Used as a singleton.
 	 * The class is marked as public so that it can be accessed from views.
@@ -102,10 +136,10 @@ public class MultiSCM extends SCM {
 			// TODO Auto-generated constructor stub
 		}
 
-		public List<SCMDescriptor<?>> getApplicableSCMs() {
+		public List<SCMDescriptor<?>> getApplicableSCMs(AbstractProject project) {
 	    	List<SCMDescriptor<?>> scms = new ArrayList<SCMDescriptor<?>>();
 	    		    	
-	    	for(SCMDescriptor<?> scm : SCM.all()) {
+	    	for(SCMDescriptor<?> scm : SCM._for(project)) {
 	    		// Filter MultiSCM itself from the list of choices.
 	    		// Theoretically it might work, but I see no practical reason to allow
 	    		// nested MultiSCM configurations.
@@ -115,23 +149,7 @@ public class MultiSCM extends SCM {
 	    	
 	    	return scms;
 	    }
-		
-	    /**
-	     * Performs on-the-fly validation of the form field 'name'.
-	     *
-	     * @param value
-	     *      This parameter receives the value that the user has typed.
-	     * @return
-	     *      Indicates the outcome of the validation. This is sent to the browser.
-	     */
-	    public FormValidation doCheckName(@QueryParameter String value) throws IOException, ServletException {
-	        if(value.length()==0)
-	            return FormValidation.error("Please set a name");
-	        if(value.length()<4)
-	            return FormValidation.warning("Isn't the name too short?");
-	        return FormValidation.ok();
-	    }
-		
+
 	    /**
 	     * This human readable name is used in the configuration screen.
 	     */
@@ -147,7 +165,19 @@ public class MultiSCM extends SCM {
 	        //  (easier when there are many fields; need set* methods for this, like setUseFrench)
 	        save();
 	        return super.configure(req,formData);
-	    }	
+	    }
+
+		@Override
+		public SCM newInstance(StaplerRequest req, JSONObject formData)
+				throws FormException {
+			return super.newInstance(req, formData);
+		}
+	    
+	}
+
+	public void save() throws IOException {
+		// TODO Auto-generated method stub
+		
 	}
 }
 
--- src/main/java/org/jenkinsci/plugins/MultiSCMChangeLogSet.java
+++ src/main/java/org/jenkinsci/plugins/MultiSCMChangeLogSet.java
@@ -1,52 +1,62 @@
 package org.jenkinsci.plugins;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 import hudson.model.AbstractBuild;
 import hudson.model.User;
 import hudson.scm.ChangeLogSet;
+import hudson.scm.ChangeLogSet.Entry;
 
 import org.jenkinsci.plugins.MultiSCMChangeLogSet.MultiSCMChangeLog;
 
 public class MultiSCMChangeLogSet extends ChangeLogSet<MultiSCMChangeLog> {
 
+	private final List<MultiSCMChangeLog> changes;
+	
 	protected MultiSCMChangeLogSet(AbstractBuild<?, ?> build) {
 		super(build);
-		// TODO Auto-generated constructor stub
+		changes = new ArrayList<MultiSCMChangeLog>();
 	}
 
 	public Iterator<MultiSCMChangeLog> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return changes.iterator();
 	}
 
 	@Override
 	public boolean isEmptySet() {
-		// TODO Auto-generated method stub
-		return false;
+		return changes.isEmpty();
 	}
-
 	
 	public static class MultiSCMChangeLog extends ChangeLogSet.Entry {
+		
+		ChangeLogSet.Entry contained;
+		
+		public MultiSCMChangeLog(ChangeLogSet.Entry containedEntry) {
+			contained = containedEntry;
+		}
 
 		@Override
 		public String getMsg() {
-			// TODO Auto-generated method stub
-			return null;
+			return contained.getMsg();
 		}
 
 		@Override
 		public User getAuthor() {
-			// TODO Auto-generated method stub
-			return null;
+			return contained.getAuthor();
 		}
 
 		@Override
 		public Collection<String> getAffectedPaths() {
-			// TODO Auto-generated method stub
-			return null;
+			return contained.getAffectedPaths();
 		}
 		
+	}
+
+	public void add(String scmClass, ChangeLogSet<? extends Entry> cls) {
+		for(ChangeLogSet.Entry e : cls)
+		changes.add(new MultiSCMChangeLog(e));
 	}
 }
