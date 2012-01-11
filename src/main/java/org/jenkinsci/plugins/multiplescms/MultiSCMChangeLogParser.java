1231be7a567515e1286cf73a1998286aa7e0390e
Kevin Bell <kbell6397@gmail.com>
2012-01-11 13:40:36 -0600

    Remove initial whitespace when extracting a log entry.
    Fixes problem with Subversion failing to parse extracted logs due to
    the <?xml...> declaration being on line 2.

--- src/main/java/org/jenkinsci/plugins/multiplescms/MultiSCMChangeLogParser.java
+++ src/main/java/org/jenkinsci/plugins/multiplescms/MultiSCMChangeLogParser.java
@@ -49,6 +49,7 @@ public class MultiSCMChangeLogParser extends ChangeLogParser {
 		private final AbstractBuild build;
 		private final File tempFile;
 		private OutputStreamWriter outputStream;
+		private boolean newStream;
 		private String scmClass;
 		
 		public LogSplitter(AbstractBuild build, String tempFilePath) {
@@ -63,7 +64,14 @@ public class MultiSCMChangeLogParser extends ChangeLogParser {
 			
 			try {
 				if(outputStream != null) {
+				    if (newStream) {
+				        while(length > 0 && Character.isWhitespace(data[startIndex])) {
+				            startIndex += 1;
+				            length -= 1;
+				        }
+				    }
 					outputStream.write(data, startIndex, length);
+					newStream = false;
 				}
 			} catch (IOException e) {
 				throw new SAXException("Could not write temp changelog file", e);
@@ -77,6 +85,7 @@ public class MultiSCMChangeLogParser extends ChangeLogParser {
 				FileOutputStream fos;
 				try {
 					scmClass = attrs.getValue("scm");
+					newStream = true;
 					fos = new FileOutputStream(tempFile);
 				} catch (FileNotFoundException e) {
 					throw new SAXException("could not create temp changelog file", e);
